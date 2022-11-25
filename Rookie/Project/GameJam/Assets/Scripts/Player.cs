7df6f71197ac42e410fd9afa2e4655b12112f5dc
polgo <45071862+godburn@users.noreply.github.com>
2022-11-25 14:48:55 +0000

    almost there

--- Rookie/Project/GameJam/Assets/Scripts/Player.cs
+++ Rookie/Project/GameJam/Assets/Scripts/Player.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 2.5f;
-    public float factor = 5f;
+    float horizontalInput;
+    float speed = 10.0f;
+    float xRange = 2.5f;
+    float factor = 5f;
+    float speedIncrease = 0.01f;
 
 
 
@@ -21,7 +22,7 @@ public class Player : MonoBehaviour {
     void Update() {
 
         bool moveMe = true;
-        horizontalInput = Input.GetAxis( "Horizontal" ) * factor * Time.deltaTime;
+        horizontalInput = Input.GetAxis( "Horizontal" ) * factor * Time.deltaTime * (speed*0.1f);
         //transform.Translate( Vector3.right * horizontalInput * Time.deltaTime * speed );
         //Debug.Log(horizontalInput);
         if (transform.position.x + horizontalInput < -xRange) {
@@ -37,18 +38,24 @@ public class Player : MonoBehaviour {
             transform.position = new Vector2( transform.position.x + horizontalInput, transform.position.y );
         }
 
+        speed += speedIncrease;
 
-        //if (Input.GetKeyDown( KeyCode.Space )) {
-            // Launch a projectile from the player
-            //Instantiate( projectilePrefab, transform.position, projectilePrefab.transform.rotation );
-        //}
-
-
+        if (Input.GetKeyDown( KeyCode.Space )) {
+            Stopper();
+        }
+        GameManager.Instance.speed = speed;
     }
-    void OnTriggerEnter2D( Collider2D col ) {
-        //if (col == null) {
+        void OnTriggerEnter2D( Collider2D col ) {
+            //if (col == null) {
             Debug.Log( col.gameObject.name + " : " + gameObject.name + " : " + Time.time );
-        //}
+            //}
+        }
+    void Stopper() {
+
+        speed = 0;
+        // change animation
     }
 
-    }
+    
+
+}
