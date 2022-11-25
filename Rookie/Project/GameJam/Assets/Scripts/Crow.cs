bedbd59a53bc7e158b963f5d0e11a0500a9b7151
polgo <45071862+godburn@users.noreply.github.com>
2022-11-25 13:47:54 +0000

    Cloud and crow ready

---
+++ Rookie/Project/GameJam/Assets/Scripts/Crow.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crow : MonoBehaviour {
+
+    public float speedFactor = 1f;
+    public float speedHorizontal = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += Vector3.left * Time.deltaTime * speedHorizontal;
+        EdgeCheck();
+    }
+    void EdgeCheck() {
+        //Debug.Log( this.transform.position.x );
+
+        if (this.transform.position.x > 5 || this.transform.position.x < -5 ) {
+
+            //speedHorizontal = 0 - speedHorizontal;
+            RandomStart();
+        }
+    }
+    void RandomStart() {
+        //int r = Random.Range( 0, 2 );
+        float s = Random.Range( 1f, 5f ) * speedFactor;
+        if (speedHorizontal < 0) {
+            //this.transform.localScale.x = -1;
+            speedHorizontal = s;
+            transform.localRotation = Quaternion.Euler( 0, 0, 0 );
+        } else {
+            speedHorizontal = -s;
+            transform.localRotation = Quaternion.Euler( 0, 180, 0 );
+        }
+
+    }
+    }
--- Rookie/Project/GameJam/Assets/Scripts/GameManager.cs
+++ Rookie/Project/GameJam/Assets/Scripts/GameManager.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameManager : MonoBehaviour { 
+
+
+    private static GameManager instance;
+
+    public static GameManager Instance {
+        get { return instance; }
+        private set { instance = value; }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static int playerHealth = 0;
+    public bool isPaused = false;
+    public float speed = 2f;
+
+    private void Awake() {
+        CheckGameManager();
+    }
+
+    void CheckGameManager() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad( gameObject );
+        } else {
+            Destroy( gameObject );
+        }
+    }
+
+    void Start() {
+        SetupLevel();
+    }
+
+    public void SetupLevel() {
+
+    }
+
+    public void CursorMode( bool _state ) {
+        if (_state) {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        } else {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
